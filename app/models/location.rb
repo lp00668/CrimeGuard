859a71d414f06d79b99227ac6d89b28730200453
lp00668 <58514448+lp00668@users.noreply.github.com>
2021-05-16 09:17:48 +0100

    Update location.rb

--- app/models/location.rb
+++ app/models/location.rb
@@ -9,16 +9,5 @@ class Location < ApplicationRecord
 	geocoded_by :address
 	after_validation :geocode, if: :address_changed?
 
-	def self.search(search)
-		if search
-			location = Location.find_by(city: search)
-			if location
-				self.where(id: location)
-			else
-				Location.all
-			end
-		else
-			Location.all
-		end
-	end
+	
 end
