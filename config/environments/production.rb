c23f84e6acc6d3afe403fef0669a6cb19d02d907
lp00668 <58514448+lp00668@users.noreply.github.com>
2021-05-17 16:28:01 +0100

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,8 +72,8 @@ Rails.application.configure do
     :address              => 'smtp.sendgrid.net',
     :port                 => '587',
     :authentication       => :plain,
-    :user_name            => 'ENV['SENDGRID_USERNAME'],
-    :password             => 'ENV['SENDGRID_PASSWORD'],
+    :user_name            => 'crimeguardapp@gmail.com',
+    :password             => 'crimeguard1',
     :domain               => 'heroku.com',
     :enable_starttls_auto => true
     }
